89f9525ceaac92006d5e5f8294be0dfaebc0ca73
José Droguett <jdroguett@produccion.datactil.com>
2016-03-27 00:50:04 -0300

    Add controller de piezas

--- app/controllers/piezas_controller.rb
+++ app/controllers/piezas_controller.rb
@@ -1,2 +1,12 @@
 class PiezasController < ApplicationController
+	def create
+    auto = Pieza.find(params[:auto_id])
+    pieza = auto.piezas.create(pieza_params)
+    respond_with auto, pieza
+  end
+
+  private
+  def pieza_params
+    params.require(:pieza).permit(:nombre,:cantidad)
+  end
 end
