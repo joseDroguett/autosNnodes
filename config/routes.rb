3ef8f01239d73d4187927122541d49c024b6b614
José Droguett <jdroguett@produccion.datactil.com>
2016-03-26 21:35:21 -0300

    add config de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
+
+  resources :autos, only: [:create, :index, :show] do
+    resources :piezas, only: [:show, :create]
+  end
 end
