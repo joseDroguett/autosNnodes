67316cd6818ea5314c229f3afa2ee182593e557b
José Droguett <jdroguett@produccion.datactil.com>
2016-03-27 04:18:47 -0300

    Add depencias de google maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 //= require angular
+//= require lodash/dist/lodash
 //= require angular-rails-templates
 //= require angular-ui-router
+//= require angular-simple-logger
+//= require angular-google-maps
 //= require_tree . 
