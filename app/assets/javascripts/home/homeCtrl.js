f0fed322beb0b4ee2fcb07134cdfa096dccc8693
José Droguett <jdroguett@produccion.datactil.com>
2016-03-28 01:58:44 -0300

    Add en la vista y controlador la funcion eliminar auto

--- app/assets/javascripts/home/homeCtrl.js
+++ app/assets/javascripts/home/homeCtrl.js
@@ -9,12 +9,21 @@ angular.module('nnodestest')
 		  autoFactory.addAuto({
 		    marca: $scope.marca,
 		    modelo: $scope.modelo,
-		    latitud: (Math.random() * 180) - 90,
+		    latitud: (Math.random() * 180) - 85,
 		    longuitud: (Math.random() * 360) - 180
 		  });
 		  $scope.cars = autoFactory.getAutos();
 		  $scope.marca = '';
 		  $scope.modelo = '';
 		};
+
+		$scope.deleteCar = function(id_auto,piezas){
+			autoFactory.borrarPiezasAutos(piezas,function(){
+				autoFactory.deleteAuto(id_auto).then(function(){
+					autoFactory.getAll();
+					$scope.cars = autoFactory.getAutos();
+				})
+			});
+		}
 	}
 ])
